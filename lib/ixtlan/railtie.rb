5245dbd8013ad0453d5fb45c6acb35611c9d0dab
Kristian Meier <m.kristian@web.de>
2012-12-04 12:14:01 +0530

    moved gettext files into own gem

--- lib/ixtlan/railtie.rb
+++ lib/ixtlan/railtie.rb
@@ -1,15 +1,9 @@
 require 'ixtlan/remote/rest'
-require 'ixtlan/gettext/manager'
 module Ixtlan
   
   class RemoteGettextRailtie < Rails::Railtie
 
     config.rest = Ixtlan::Remote::Rest.new
-    
-    config.gettext = Ixtlan::Gettext::Manager.new
-    
-    rake_tasks do
-      load 'ixtlan/gettext/translation.rake'
-    end
+
   end
 end
