d1a4dd6a23f46c10a54fe8060894fe83d347713b
Kristian Meier <m.kristian@web.de>
2012-12-05 22:58:37 +0530

    fixed require / modules etc

--- lib/ixtlan/remote/summary.rb
+++ lib/ixtlan/remote/summary.rb
@@ -1,23 +1,25 @@
-class Ixtlan::Remote::Sync
+module Ixtlan
+  module Remote
+    class Summary
 
-  class Summary
-
-    def initialize(clazz)
-      @count = 0
-      @failures = []
-      @clazz = clazz
-    end
-
-    def inc_count
-      @count += 1
-    end
-
-    def inc_failures(errors)
-      @failures << errors.inspect
-    end
-
-    def to_log
-      "update #{@clazz} - total: #{@count + @failures.size}  success: #{@count}  failures: #{@failures.size == 0 ? 0 : @failures.join("\n\t\t")}"
+      def initialize(clazz)
+        @count = 0
+        @failures = []
+        @clazz = clazz
+      end
+      
+      def inc_count
+        @count += 1
+      end
+      
+      def inc_failures(errors)
+        @failures << errors.inspect
+      end
+      
+      def to_log
+        "update #{@clazz} - total: #{@count + @failures.size}  success: #{@count}  failures: #{@failures.size == 0 ? 0 : @failures.join("\n\t\t")}"
+      end
     end
   end
 end
+
--- lib/ixtlan/remote/sync.rb
+++ lib/ixtlan/remote/sync.rb
@@ -1,80 +1,84 @@
 require 'ixtlan/remote/summary'
 require 'active_support/all'
 
-class Ixtlan::Remote::Sync
+module Ixtlan
+  module Remote
+    class Sync
 
-  SECONDS_IN_DAY = 60 * 60 * 24
-  NANOSECONDS_IN_DAY = SECONDS_IN_DAY * 1000 * 1000 * 1000
-  def initialize(restserver)
-    @restserver = restserver
-  end
+      SECONDS_IN_DAY = 60 * 60 * 24
+      NANOSECONDS_IN_DAY = SECONDS_IN_DAY * 1000 * 1000 * 1000
+      def initialize(restserver)
+        @restserver = restserver
+      end
 
-  def clazzes
-    @clazzes ||= {}
-  end
-  private :clazzes
+      def clazzes
+        @clazzes ||= {}
+      end
+      private :clazzes
 
-  def register(clazz, &block)
-    clazzes[clazz] = block unless clazzes.key?(clazz)
-  end
+      def register(clazz, &block)
+        clazzes[clazz] = block unless clazzes.key?(clazz)
+      end
 
-  # max method ORM dependent
-  if defined? ActiveRecord
-    def max(clazz)
-      (clazz.maximum(:updated_at) || DateTime.new(0)).to_datetime
-    end
-  else
-    def max(clazz)
-      clazz.max(:updated_at)
-    end
-  end
-  private :max
-
-  # UTC timestamp of last updated record
-  def last_update( clazz )
-    last_date = ( max( clazz ) || DateTime.new( 0 ) )
-    last_date.strftime( '%Y-%m-%d %H:%M:%S.' ) + ( "%06d" % ( last_date.sec_fraction / NANOSECONDS_IN_DAY / 1000 ) ) + "+0:00"
-  end
-  private :last_update
-
-  def self.do_it( clazz = nil )
-    if clazz
-      new.do_it( clazz )
-    else
-      new.do_it
-    end
-  end
-
-  # load the last changed records
-  def do_it(set = clazzes.keys)
-    @last_result = []
-    set = [set] unless set.is_a? Array
-    # use only regstered classes !!
-    set = set - (set - clazzes.keys)
-    set.to_a.each do |clazz|
-      summary = Summary.new(clazz)
-      @last_result << summary
-      @restserver.retrieve(clazz, 
-                           :last_changes, 
-                           :updated_at => last_update(clazz)).each do |item|
-        if item.save
-          summary.inc_count
-        else
-          summary.inc_failures(item.errors)
+      # max method ORM dependent
+      if defined? ActiveRecord
+        def max(clazz)
+          (clazz.maximum(:updated_at) || DateTime.new(0)).to_datetime
+        end
+      else
+        def max(clazz)
+          clazz.max(:updated_at)
         end
       end
-      b = clazzes[clazz]
-      b.call summary if b
-    end
-    to_log
-  end
+      private :max
 
-  def to_log
-    if @last_result
-      @last_result.collect { |r| r.to_log }.join("\n\t")
-    else
-      "no results yet"
+      # UTC timestamp of last updated record
+      def last_update( clazz )
+        last_date = ( max( clazz ) || DateTime.new( 0 ) )
+        last_date.strftime( '%Y-%m-%d %H:%M:%S.' ) + ( "%06d" % ( last_date.sec_fraction / NANOSECONDS_IN_DAY / 1000 ) ) + "+0:00"
+      end
+      private :last_update
+
+      def self.do_it( clazz = nil )
+        if clazz
+          new.do_it( clazz )
+        else
+          new.do_it
+        end
+      end
+
+      # load the last changed records
+      def do_it(set = clazzes.keys)
+        @last_result = []
+        set = [set] unless set.is_a? Array
+        # use only regstered classes !!
+        set = set - (set - clazzes.keys)
+        set.to_a.each do |clazz|
+          summary = Summary.new(clazz)
+          @last_result << summary
+          @restserver.retrieve(clazz, 
+                               :last_changes, 
+                               :updated_at => last_update(clazz)).each do |item|
+            if item.save
+              summary.inc_count
+            else
+              summary.inc_failures(item.errors)
+            end
+          end
+          b = clazzes[clazz]
+          b.call summary if b
+        end
+        to_log
+      end
+
+      def to_log
+        if @last_result
+          @last_result.collect { |r| r.to_log }.join("\n\t")
+        else
+          "no results yet"
+        end
+      end
+      alias :to_s :to_log
     end
   end
-  alias :to_s :to_log
 end
