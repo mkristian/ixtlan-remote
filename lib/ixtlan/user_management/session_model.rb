eb8fa7f51e679a13c417df8cf83bd59e71c7cc54
Kristian Meier <m.kristian@web.de>
2012-12-29 14:07:14 +0530

    to_s

--- lib/ixtlan/user_management/session_model.rb
+++ lib/ixtlan/user_management/session_model.rb
@@ -27,6 +27,10 @@ module Ixtlan
       attribute :idle_session_timeout, Integer
       attribute :user, User
       attribute :permissions, Array[Object]
+
+      def to_s
+        "Session( #{user.name}<#{user.login}> groups[ #{user.groups.collect { |g| g.name }.join ',' } ] #{idle_session_timeout} )"
+      end
     end
   end
 end
